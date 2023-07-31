89e48b52c8f91243b9a799ddcfcb8b1421dc03a4
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-07-31 19:16:01 +0500

    get and put data done

--- server.mjs
+++ server.mjs
@@ -1,4 +1,6 @@
 import express from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { customAlphabet } from 'nanoid';
 const nanoid = customAlphabet('1234567890', 20)
 
@@ -12,7 +14,9 @@ const productsCollection = database.collection('products');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my Shop");
@@ -36,14 +40,27 @@ const products = [
 ];
 
 
-  app.get("/products", (req, res) => {
-    res.send(
-        {
-            message: "All products list",
-            data: products,
-        }
-    );
+
+// Get All Data
+  // app.get("/products", (req, res) => {
+  //   res.send(
+  //       {
+  //           message: "All products list",
+  //           data: products,
+  //       }
+  //   );
+  // });
+
+  app.get("/products", async (req, res) => {
+    try {
+      const result = await productsCollection.find({}).toArray();
+      res.send(result); 
+    } catch (error) {
+      console.error("Error getting products:", error);
+      res.status(500).send("Error getting products");
+    }
   });
+
 
 
 //   Get Single product
@@ -108,7 +125,7 @@ app.post("/product", async (req, res) => {
     description: req.body.description,
   }
   const result = await productsCollection.insertOne(doc);
-  
+
   res.status(201).send({ message: "created product" });
 });
 
